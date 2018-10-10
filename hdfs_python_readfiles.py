08beab1999546a8c57c1364799ba13f16245148e
Anil Kumar <36426477+AnilKumar3@users.noreply.github.com>
2018-10-09 17:04:02 -0700

    Add files via upload

---
+++ hdfs_python_readfiles.py
@@ -0,0 +1,26 @@
+#
+#import sys
+import subprocess
+#function to call shell command
+def run_cmd(args_list):
+        #run linux commands
+        print('Running system command: {0}'.format(' '.join(args_list)))
+        proc = subprocess.Popen(args_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        s_output, s_err = proc.communicate()
+        s_return =  proc.returncode
+        return s_return, s_output, s_err
+
+#Run Hadoop ls command in Python to get all the files
+(ret, out, err)= run_cmd(['hdfs', 'dfs', '-ls', '/raw/LOGGING/hdp_application_audit/2018/10/07/'])
+for lines in out.split('\n'):
+        file1 = '/'+lines.split(" /")[-1]
+        print file1
+        print ""
+        print ""
+        #To get content of the file
+        (ret1,out1,err1)=run_cmd(['hdfs', 'dfs', '-text',file1])
+        for jlines in out1.split('\n'):
+                jfile1=jlines.split("\t")[-1]
+                print jfile1
+
+
